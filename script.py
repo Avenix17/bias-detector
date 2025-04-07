cf09d89630dd8ffb4285c500f13a6014938e89d3
Avyrie Fellows <avyriefellows@gmail.com>
2025-04-07 08:41:01 -0600

    Website updated to take user input within the website instead of terminal. Added results.html
    Co-authored-by: CierraL <CierraL@users.noreply.github.com>
    Co-authored-by: LydiaPorter <LydiaPorter@users.noreply.github.com>
    Co-authored-by: mjens328 <mjens328@users.noreply.github.com>

--- script.py
+++ script.py
@@ -10,8 +10,7 @@ from textblob import TextBlob
 import requests
 
 # Flask is used to create the html page
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -140,80 +139,94 @@ def detect_fake_news(text):
 
 
 # Method/class calls
-user_input = input("Enter text: ")
-blob = TextBlob(user_input)
-blob.sentiment
-bias = blob.sentiment.subjectivity # Number for Subjectivity (Bias)
-polarity = blob.sentiment.polarity # Number for Polarity (+/-)
-prediction = detect_fake_news(user_input) 
-
-# if statements for bias/polarity/news
-# Bias
-if 0 <= bias < .2:
-    determined_bias = BiasFactory.determine_bias("ExLowBias")
-elif .2 <= bias < .4:
-    determined_bias = BiasFactory.determine_bias("LowBias")
-elif .4 <= bias < .6:
-    determined_bias = BiasFactory.determine_bias("ModerateBias")
-elif .6 <= bias < .8:
-    determined_bias = BiasFactory.determine_bias("HighBias")
-elif .8 <= bias <= 1:
-    determined_bias = BiasFactory.determine_bias("ExBias")
-
-# Polarity
-if -1 <= polarity < -0.333:
-    determined_polarity = PolarityFactory.determine_polarity("Negative")
-elif -0.333 <= polarity < 0.333:
-    determined_polarity = PolarityFactory.determine_polarity("Neutral")
-elif 0.333 <= polarity <= 1:
-    determined_polarity = PolarityFactory.determine_polarity("Positive")
-
-# News
-if prediction == "Fake News":
-   determined_prediction = FakeNewsFactory.determine_fake("Fake News")
-elif prediction == "Not Fake News":
-   determined_prediction = FakeNewsFactory.determine_fake("Not Fake News")
+# blob = TextBlob(user_input)
+# blob.sentiment
+# bias = blob.sentiment.subjectivity # Number for Subjectivity (Bias)
+# polarity = blob.sentiment.polarity # Number for Polarity (+/-)
+# prediction = detect_fake_news(user_input)
 
 
-determined_bias.determine_bias()
-determined_polarity.determine_polarity()
-determined_prediction.determine_fake()
+def analysis(input):
+    blob = TextBlob(input)
+    blob.sentiment
+    bias = blob.sentiment.subjectivity # Number for Subjectivity (Bias)
+    polarity = blob.sentiment.polarity # Number for Polarity (+/-)
+    prediction = detect_fake_news(input)
 
-# Flask takes variables to place into html template
+    # if statements for bias/polarity/news
+    # Bias
+    if 0 <= bias < .2:
+        determined_bias = BiasFactory.determine_bias("ExLowBias")
+    elif .2 <= bias < .4:
+        determined_bias = BiasFactory.determine_bias("LowBias")
+    elif .4 <= bias < .6:
+        determined_bias = BiasFactory.determine_bias("ModerateBias")
+    elif .6 <= bias < .8:
+        determined_bias = BiasFactory.determine_bias("HighBias")
+    elif .8 <= bias <= 1:
+        determined_bias = BiasFactory.determine_bias("ExBias")
+
+    # Polarity
+    if -1 <= polarity < -0.333:
+        determined_polarity = PolarityFactory.determine_polarity("Negative")
+    elif -0.333 <= polarity < 0.333:
+        determined_polarity = PolarityFactory.determine_polarity("Neutral")
+    elif 0.333 <= polarity <= 1:
+        determined_polarity = PolarityFactory.determine_polarity("Positive")
+
+    # News
+    if prediction == "Fake News":
+        determined_prediction = FakeNewsFactory.determine_fake("Fake News")
+    elif prediction == "Not Fake News":
+        determined_prediction = FakeNewsFactory.determine_fake("Not Fake News")
+
+
+    return determined_bias.determine_bias(), determined_polarity.determine_polarity(), determined_prediction.determine_fake()
+
+
 @app.route('/')
 def index():
+    return render_template("index.html")
+
+@app.route('/results', methods=['POST'])
+def results():
+
+    if request.method == 'POST':
+        user_input = request.form['user_input']
+        bias, polarity, prediction = analysis(user_input)
+
     context = {
-    'bias': determined_bias.determine_bias(),
-    'polarity': determined_polarity.determine_polarity(),
-    'prediction': determined_prediction.determine_fake()
+    'bias': bias,
+    'polarity': polarity,
+    'prediction': prediction
     }
 
-    return render_template("index.html", **context)
+    return render_template("results.html", **context)
 
 
 # UNIT TEST SUITE
-import sys
+# import sys
 
-def test(did_pass):
-    """ Print the result of a test. """
-    linenum = sys._getframe(1).f_lineno # Get the caller’s line number.
-    if did_pass:
-        msg = "Test at line {0} ok.".format(linenum)
-    else:
-        msg = ("Test at line {0} FAILED.".format(linenum))
-    print(msg)
+# def test(did_pass):
+#     """ Print the result of a test. """
+#     linenum = sys._getframe(1).f_lineno # Get the caller’s line number.
+#     if did_pass:
+#         msg = "Test at line {0} ok.".format(linenum)
+#     else:
+#         msg = ("Test at line {0} FAILED.".format(linenum))
+#     print(msg)
 
-def test_suite():
-    # bias
-    test(ClassBiased("The moon landing was fake") == ClassBiased(ExBias))
-    test(user_input(123456789) == None)
+# def test_suite():
+#     # bias
+#     test(ClassBiased("The moon landing was fake") == ClassBiased(ExBias))
+#     test(user_input(123456789) == None)
 
-    # polarity
-    test(user_input("I love unicorns so much") == Positive)
-    test(user_input("The moon landing was fake") == Neutral)
-    test(user_input(123456789) == None)
+#     # polarity
+#     test(user_input("I love unicorns so much") == Positive)
+#     test(user_input("The moon landing was fake") == Neutral)
+#     test(user_input(123456789) == None)
 
-    # fake news
+#     # fake news
     
-#test()
-test_suite()
+# #test()
+# test_suite()
